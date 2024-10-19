5422d3d4ea9a5fca5e5d9d4536a5b8d02840fe77
Ayush <119443185+CrackedResearcher@users.noreply.github.com>
2024-10-20 00:18:41 +0530

    fixed the landing page ui for card section, animations, logo to be added

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,11 @@
+import PrivacyPolicy from "@/components/PrivacyPolicy/PrivacyPolicyPage";
+
+const PrivacyPage = () => {
+  return (
+    <div>
+        <PrivacyPolicy />
+    </div>
+  );
+};
+
+export default PrivacyPage;
--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,19 +1,23 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { Check } from "lucide-react"
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { Check } from "lucide-react";
+import { cn } from "../../lib/utils";
 
-import { cn } from "../../lib/utils"
+// Define a new interface that extends CheckboxPrimitive's props
+interface CheckboxProps extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {
+  borderColor?: string; // Adding borderColor as an optional prop
+}
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
+  CheckboxProps // Use the newly defined CheckboxProps type
+>(({ className, borderColor = "border-white", ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer phone:size-4 lg:size-[18px] shrink-0 rounded-[5px] card-cover shadow focus-visible:outline-none border border-white disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-gradient-to-br data-[state=checked]:from-blue-500 data-[state=checked]:via-blue-600 data-[state=checked]:to-blue-700 data-[state=checked]:text-neutral-50 data-[state=checked]:border-0",
+      `peer phone:size-4 lg:size-[18px] shrink-0 rounded-[5px] card-cover shadow focus-visible:outline-none border ${borderColor} disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-gradient-to-br data-[state=checked]:from-blue-500 data-[state=checked]:via-blue-600 data-[state=checked]:to-blue-700 data-[state=checked]:text-neutral-50 data-[state=checked]:border-0`,
       className
     )}
     {...props}
@@ -24,7 +28,8 @@ const Checkbox = React.forwardRef<
       <Check className="h-[14px] w-[14px]" strokeWidth={2} />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
-))
-Checkbox.displayName = CheckboxPrimitive.Root.displayName
+));
 
-export { Checkbox }
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox };
