e191d3ad3ee16e9409a03305a54000f59675181e
Ayush <119443185+CrackedResearcher@users.noreply.github.com>
2024-11-13 19:54:45 +0530

    minor cahnges added suspense to fix prerendering issues

--- src/app/upload-mca-docs/page.tsx
+++ src/app/upload-mca-docs/page.tsx
@@ -1,17 +1,17 @@
-"use client";
+'use client';
 
-import React, { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-import { useDropzone } from "react-dropzone";
-import { Button } from "../../components/ui/button";
-import { Input } from "../../components/ui/input";
-import { useToast } from "../../utils/show-toasts";
+import React, { useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { useDropzone } from 'react-dropzone';
+import { Button } from '../../components/ui/button';
+import { Input } from '../../components/ui/input';
+import { useToast } from '../../utils/show-toasts';
 import { Suspense } from 'react';
 
-const Page = () => {
-  const searchParams = useSearchParams(); 
-  const queryCin = searchParams.get("cin") || ""; 
-  const queryUserId = searchParams.get("userId") || ""; 
+function Content() {
+  const searchParams = useSearchParams();
+  const queryCin = searchParams.get('cin') || '';
+  const queryUserId = searchParams.get('userId') || '';
 
   // Initialize state directly with query values
   const [cin, setCin] = useState(queryCin);
@@ -22,15 +22,13 @@ const Page = () => {
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop: (acceptedFiles: File[]) => {
-      setFiles((prevFiles) => [...prevFiles, ...acceptedFiles]);
+      setFiles(prevFiles => [...prevFiles, ...acceptedFiles]);
     },
     multiple: true,
   });
 
   const handleRemoveFile = (indexToRemove: number) => {
-    setFiles((prevFiles) =>
-      prevFiles.filter((_, index) => index !== indexToRemove)
-    );
+    setFiles(prevFiles => prevFiles.filter((_, index) => index !== indexToRemove));
   };
 
   const handleSubmit = async (event: React.FormEvent) => {
@@ -38,65 +36,62 @@ const Page = () => {
 
     if (!cin || !userId) {
       showToast({
-        message: "CIN and User ID are required",
-        type: "error",
+        message: 'CIN and User ID are required',
+        type: 'error',
       });
       return;
     }
 
     const formData = new FormData();
-    formData.append("cin", cin);
-    formData.append("user_id", userId);
+    formData.append('cin', cin);
+    formData.append('user_id', userId);
 
-    files.forEach((file) => {
-      formData.append("files[]", file);
+    files.forEach(file => {
+      formData.append('files[]', file);
     });
 
     try {
       const response = await fetch(`${apiUrl}/mcadocs-api/upload/`, {
-        method: "POST",
+        method: 'POST',
         body: formData,
       });
 
       if (response.ok) {
         showToast({
-          message: "Uploaded the documents successfully!",
-          type: "success",
+          message: 'Uploaded the documents successfully!',
+          type: 'success',
         });
         // Clear form state after successful submission
-        setCin("");
-        setUserId("");
+        setCin('');
+        setUserId('');
         setFiles([]);
       } else {
         showToast({
-          message: "Please retry, document upload failed..",
-          type: "error",
+          message: 'Please retry, document upload failed..',
+          type: 'error',
         });
       }
-    } catch (error:any) {
+    } catch (error: any) {
       showToast({
         message: `An error occurred: ${error.message}`,
-        type: "error",
+        type: 'error',
       });
     }
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
     <div className="flex justify-center items-center min-h-screen">
       <div className="[background:linear-gradient(269.75deg,_#011049,_#19112f_25.75%,_#251431_51.79%,_#301941_64.24%,_#6e3050)] shadow-md rounded-lg p-6 w-full max-w-lg">
         <h1 className="text-2xl font-semibold mb-4 text-center text-zinc-100">
           Upload MCA Documents
         </h1>
         <p className="mb-6 text-center text-sm text-zinc-400">
-        Please upload the required documents for this company. For your convenience, the CIN & UserID have already been pre-filled from the URL.
+          Please upload the required documents for this company. For your convenience, the CIN &
+          UserID have already been pre-filled from the URL.
         </p>
         <form className="space-y-4" onSubmit={handleSubmit}>
           <div className="form-group">
-            <label
-              htmlFor="cin"
-              className="block text-sm font-medium text-zinc-400 mb-1"
-            >
+            <label htmlFor="cin" className="block text-sm font-medium text-zinc-400 mb-1">
               CIN
             </label>
             <Input
@@ -107,16 +102,13 @@ const Page = () => {
               required
               value={cin}
               disabled={!!cin}
-              onChange={(e) => setCin(e.target.value)}
+              onChange={e => setCin(e.target.value)}
               className="w-full bg-transparent text-zinc-300 placeholder-zinc-400 focus:outline-none focus:ring focus:ring-blue-500"
             />
           </div>
 
           <div className="form-group">
-            <label
-              htmlFor="userId"
-              className="block text-sm font-medium text-zinc-400 mb-1"
-            >
+            <label htmlFor="userId" className="block text-sm font-medium text-zinc-400 mb-1">
               User ID
             </label>
             <Input
@@ -127,7 +119,7 @@ const Page = () => {
               required
               value={userId}
               disabled={!!userId}
-              onChange={(e) => setUserId(e.target.value)}
+              onChange={e => setUserId(e.target.value)}
               className="w-full bg-transparent text-zinc-300 placeholder-zinc-400 focus:outline-none focus:ring focus:ring-blue-500"
             />
           </div>
@@ -139,7 +131,7 @@ const Page = () => {
             <div
               {...getRootProps({
                 className:
-                  "dropzone cursor-pointer flex items-center justify-center p-4 border-2 border-dashed border-gray-500 rounded-md bg-transparent hover:bg-black/30 transition",
+                  'dropzone cursor-pointer flex items-center justify-center p-4 border-2 border-dashed border-gray-500 rounded-md bg-transparent hover:bg-black/30 transition',
               })}
             >
               <input {...getInputProps()} />
@@ -153,10 +145,7 @@ const Page = () => {
                 <p className="text-zinc-200 mt-3">Selected files:</p>
                 <ul>
                   {files.map((file, index) => (
-                    <li
-                      key={index}
-                      className="flex items-center justify-between mb-1"
-                    >
+                    <li key={index} className="flex items-center justify-between mb-1">
                       <span>{file.name}</span>
                       <button
                         type="button"
@@ -181,6 +170,13 @@ const Page = () => {
         </form>
       </div>
     </div>
+  );
+}
+
+const Page = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Content />
     </Suspense>
   );
 };
