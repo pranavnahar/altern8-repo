35b120517cfdb3da79e352953f393893c47c3554
Allen-Saji <allensaji04@gmail.com>
2025-01-06 13:17:04 +0530

    remove console.log

--- src/app/(dashboard)/ledger/page.tsx
+++ src/app/(dashboard)/ledger/page.tsx
@@ -1,30 +1,24 @@
-import BasicTable from "@/components/global/basic-table"
-import { getLedgerDetails } from "./actions"
-import TransactionSheet from "./components/transaction-sheet"
-import { accountsColumns } from "./columns"
-import TransactionUpload from "./components/transaction-upload"
-import TransactionDownload from "./components/transcation-download"
+import BasicTable from '@/components/global/basic-table';
+import { getLedgerDetails } from './actions';
+import TransactionSheet from './components/transaction-sheet';
+import { accountsColumns } from './columns';
+import TransactionUpload from './components/transaction-upload';
+import TransactionDownload from './components/transcation-download';
 
 const Page = async () => {
-  const { accounts, otherAccounts, trancheIDs } = await getLedgerDetails()
-  console.log("ledeger detials are as follows: ", accounts)
-
-  const allAccounts = [...accounts, ...otherAccounts]
+  const { accounts, otherAccounts, trancheIDs } = await getLedgerDetails();
+  const allAccounts = [...accounts, ...otherAccounts];
 
   return (
     <div className="min-h-screen">
       <div className="grid grid-cols-3">
         <div></div>
         <h1 className="text-5xl text-center font-relative-medium text-white-font">Accounts</h1>
-       <div className="flex items-center gap-5">
-        <TransactionUpload />
-        <TransactionDownload /> 
-        <TransactionSheet
-          accounts={allAccounts}
-          trancheIds={trancheIDs}
-          mode="add"
-        />
-       </div>
+        <div className="flex items-center gap-5">
+          <TransactionUpload />
+          <TransactionDownload />
+          <TransactionSheet accounts={allAccounts} trancheIds={trancheIDs} mode="add" />
+        </div>
       </div>
       <BasicTable
         data={accounts}
@@ -34,7 +28,7 @@ const Page = async () => {
         tableName="ledger-accounts-view-table"
       />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
